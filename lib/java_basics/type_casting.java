89c4a071ccf8142807604de0ef05a078114948d1
Hossam Ramadan <www.rhossam882@gmail.com>
2022-02-28 01:15:05 +0200

    basicsForToday

---
+++ lib/java_basics/type_casting.java
@@ -0,0 +1,17 @@
+package java_basics;
+
+ class typecast {
+ public static void main(String[] args) {
+     
+    int x = 2;
+    float y;
+    double z;
+    x = (int)2.8;
+    y = (float) 2;
+
+    String num = "10";
+    x = Integer.parseInt(num);
+    System.out.println(x + 4);
+    System.out.println(y);
+ }
+}
---
+++ lib/java_basics/variables.java
@@ -0,0 +1,23 @@
+package java_basics;
+
+public class variables {
+    public static void main(String[] args) {
+        int variable1 = 5;
+        String variable2 = "Hello World";
+        char variable3 = 'h';
+        double variable4 = 5.4;
+        float variable5 = 10.4f;
+        boolean variable6 = false;
+    
+        int a = 3, b = 4;
+        int c = a + b;
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+        System.out.println(variable6);
+        System.out.println(c);
+
+    }
+}
