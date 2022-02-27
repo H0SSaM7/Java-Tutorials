378eb858777cff5a7cd6711bf35c762b27f08115
Hossam Ramadan <www.rhossam882@gmail.com>
2022-02-27 22:58:17 +0200

    output

---
+++ lib/java_basics/output.java
@@ -0,0 +1,12 @@
+package java_basics;
+
+public class output {
+    public static void  main(String[] arg){
+        System.out.println("Hello, Hossam");
+        System.out.println("Hello " + "Hossam");
+        System.out.println("Hello, Hosssam \nHow are you?");
+        System.out.println(5 + 5); // output will be 10
+        System.out.println("5 + 5"); // just print 
+    }
+    
+}
