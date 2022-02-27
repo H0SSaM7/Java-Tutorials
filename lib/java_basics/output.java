b9ea3f6cc5cd19141f07d858ae5a020d0433c574
Hossam Ramadan <www.rhossam882@gmail.com>
2022-02-27 23:05:41 +0200

    test

--- lib/java_basics/output.java
+++ lib/java_basics/output.java
@@ -4,7 +4,7 @@ public class output {
     public static void  main(String[] arg){
         System.out.println("Hello, Hossam");
         System.out.println("Hello " + "Hossam");
-        System.out.println("Hello, Hosssam \nHow are you?");
+        System.out.println("Hello, Hosssam \nHow are you?"); // \n to print words after it in the next line
         System.out.println(5 + 5); // output will be 10
         System.out.println("5 + 5"); // just print 
     }
